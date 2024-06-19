7053e12583b435f06b85971157b7d5ddf0a812dd
Darshan S <darshan0189@gmail.com>
2024-06-19 11:20:21 +0530

    Added ecs code

--- main.tf
+++ main.tf
@@ -1,15 +1,3 @@
-provider "aws" {
-    region = "us-east-1"  
-}
-
-resource "aws_instance" "foo" {
-  ami           = "ami-05fa00d4c63e32376" # us-west-2
-  instance_type = "t2.micro"
-  tags = {
-      Name = "TF-Instance"
-  }
-}
-
 resource "aws_ecs_cluster" "ecs-cluster" {
   name = "aws-ecs"
 
