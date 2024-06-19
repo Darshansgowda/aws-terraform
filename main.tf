16a4dc3433e384a3388bbc50b6e6169ef912600f
Darshan S <darshan0189@gmail.com>
2024-06-19 12:05:22 +0530

    Destroy ecs code

--- main.tf
+++ main.tf
@@ -2,12 +2,11 @@ provider "aws" {
     region = "us-east-1"  
 }
 
-resource "aws_ecs_cluster" "ecs-cluster" {
-  name = "aws-ecs"
-
-  setting {
-    name  = "containerInsights"
-    value = "enabled"
-  }
-}
+#resource "aws_ecs_cluster" "ecs-cluster" {
+#  name = "aws-ecs"
+#  setting {
+#    name  = "containerInsights"
+#    value = "enabled"
+#  }
+#}
 
